fd74cbb4b55da03080c57395bb41b9a25c1c8113
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2024-06-12 11:00:47 -0300

    basic snapshot creation and reading

---
+++ src/snapshot_test.go
@@ -0,0 +1,74 @@
+package snapshot
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateSnapshot(t *testing.T) {
+	t.Parallel()
+
+	writer, err := NewSnapshotWriter("./tmp")
+	require.NoError(t, err)
+
+	// Generate random resources.
+	datasources := generateItems(string(DatasourceDataType), 10_000)
+
+	folders := generateItems(string(FolderDataType), 10_000)
+
+	dashboards := generateItems(string(DashboardDataType), 10_000)
+
+	// Write the resources to the snapshot.
+	require.NoError(t, writer.Write(string(DatasourceDataType), datasources))
+	require.NoError(t, writer.Write(string(FolderDataType), folders))
+	require.NoError(t, writer.Write(string(DashboardDataType), dashboards))
+
+	// Write the index file.
+	indexFilePath, err := writer.Finish()
+	require.NoError(t, err)
+
+	file, err := os.Open(indexFilePath)
+	require.NoError(t, err)
+
+	index, err := ReadIndex(file)
+	require.NoError(t, err)
+
+	resources := make(map[string][]MigrateDataRequestItemDTO)
+
+	// Using the index, read each data file and group the contents by resource type (e.g. dashboards).
+	for resourceType, filePaths := range index.Items {
+		for _, filePath := range filePaths {
+			file, err := os.Open(filePath)
+			require.NoError(t, err)
+
+			partition, err := ReadFile(file)
+			require.NoError(t, err)
+
+			resources[resourceType] = append(resources[resourceType], partition.Items...)
+		}
+	}
+
+	// Ensure we got the initial data back.
+	assert.Equal(t, datasources, resources[string(DatasourceDataType)])
+	assert.Equal(t, folders, resources[string(FolderDataType)])
+	assert.Equal(t, dashboards, resources[string(DashboardDataType)])
+}
+
+func generateItems(resourceType string, numItems int) []MigrateDataRequestItemDTO {
+	items := make([]MigrateDataRequestItemDTO, 0, numItems)
+
+	for i := 0; i < numItems; i++ {
+		items = append(items, MigrateDataRequestItemDTO{
+			Type:  MigrateDataType(resourceType),
+			RefID: fmt.Sprintf("%s_%d_ref_id", resourceType, i),
+			Name:  fmt.Sprintf("%s_%d_name", resourceType, i),
+			Data:  fmt.Sprintf("%s_%d_data", resourceType, i),
+		})
+	}
+
+	return items
+}
