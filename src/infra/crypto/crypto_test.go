4c30c1b40b06d6bf81a75fe6902db010ccfb373d
poorlydefinedbehaviour <brunotj2015@hotmail.com>
2026-05-26 11:24:42 +0200

    crypto: check encrypted payload slice length before using it

--- src/infra/crypto/crypto_test.go
+++ src/infra/crypto/crypto_test.go
@@ -14,38 +14,47 @@ import (
 func TestNacl(t *testing.T) {
 	t.Parallel()
 
-	nacl := NewNacl()
+	t.Run("basic", func(t *testing.T) {
+		nacl := NewNacl()
 
-	sender, err := nacl.GenerateKeys()
-	require.NoError(t, err)
+		sender, err := nacl.GenerateKeys()
+		require.NoError(t, err)
 
-	recipient, err := nacl.GenerateKeys()
-	require.NoError(t, err)
+		recipient, err := nacl.GenerateKeys()
+		require.NoError(t, err)
 
-	msg := "hello world"
+		msg := "hello world"
 
-	var nonce [24]byte
-	_, err = io.ReadFull(cryptoRand.Reader, nonce[:])
-	require.NoError(t, err)
+		var nonce [24]byte
+		_, err = io.ReadFull(cryptoRand.Reader, nonce[:])
+		require.NoError(t, err)
 
-	reader, err := nacl.Encrypt(contracts.AssymetricKeys{
-		Public:  recipient.Public,
-		Private: sender.Private,
-	},
-		strings.NewReader(msg),
-	)
-	require.NoError(t, err)
+		reader, err := nacl.Encrypt(contracts.AssymetricKeys{
+			Public:  recipient.Public,
+			Private: sender.Private,
+		},
+			strings.NewReader(msg),
+		)
+		require.NoError(t, err)
 
-	reader, err = nacl.Decrypt(contracts.AssymetricKeys{
-		Public:  sender.Public,
-		Private: recipient.Private,
-	},
-		reader,
-	)
-	require.NoError(t, err)
+		reader, err = nacl.Decrypt(contracts.AssymetricKeys{
+			Public:  sender.Public,
+			Private: recipient.Private,
+		},
+			reader,
+		)
+		require.NoError(t, err)
 
-	buffer, err := io.ReadAll(reader)
-	require.NoError(t, err)
+		buffer, err := io.ReadAll(reader)
+		require.NoError(t, err)
 
-	assert.Equal(t, msg, string(buffer))
+		assert.Equal(t, msg, string(buffer))
+	})
+
+	t.Run("decrypt returns error when encrypted payload is too small", func(t *testing.T) {
+		nacl := NewNacl()
+
+		_, err := nacl.Decrypt(contracts.AssymetricKeys{}, strings.NewReader("too small"))
+		require.ErrorIs(t, err, ErrEncryptedPayloadLengthTooSmall)
+	})
 }
